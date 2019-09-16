6abc5d10d2df8b06e8fe94563bdcca736f2883e8
jaravellzie <53119278+jaravellzie@users.noreply.github.com>
2019-09-16 10:59:59 +0800

    Add files via upload

---
+++ LectureThree.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+	<title>Table Exercise</title>
+</head>
+<body>
+
+<table align="center" border=1 cellpadding=10 cellspacing=5 height=600 width=1250>
+	<tr>
+		<td colspan=4></td>
+	</tr>
+
+	<tr>
+		<td>Home</td>
+		<td>About Us</td>
+		<td>Contact Us</td>
+		<td>Link</td>
+	</tr>
+
+	<tr>
+		<td colspan=1 rowspan=3></td>
+		<td colspan=2 rowspan=3></td>
+		<td colspan=1 rowspan=1></td>
+	</tr>
+
+	<tr>
+		<td></td>
+	</tr>
+
+	<tr>
+		<td></td>
+	</tr>
+
+	<tr>
+		<td colspan=4>Footer</td>
+	</tr>
+
+</table>
+
+</body>
+</html>
